ac728f89016b00f77ab3eece46d98368da48f9ca
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-02-04 16:08:54 -0600

    completed color alert portion of exercise

--- js/external.js
+++ js/external.js
@@ -1,3 +1,9 @@
 "use strict";
 
 console.log("Hello from external JavaScript");
+
+
+alert('Welcome to my Website!');
+
+var userInput = prompt('What is your favorite color?');
+alert('Great, ' + userInput + ' is my favorite color too!' );

956474796537789c7b1547d4d766710afb71634b
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-02-11 12:26:39 -0600

    stopping point

--- js/while.js
+++ js/while.js
@@ -1,9 +1,23 @@
 "use strict";
 (function () {
-    var i = 2;
 
-    while (i < 16) {
-        console.log(Math.pow(i, 2));
-        i++;
+
+
+    var x = 2;
+
+    while(x <= 65536){
+        console.log(x);
+        x *= 2;
     }
+
+
+
+
+    // An ice cream seller can't go home until she sells all of her cones. Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell. Your code should generate numbers between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is how you get the random numbers.
+
+
+
+
+
+
 })();

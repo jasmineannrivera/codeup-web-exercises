6c5bb36f19306c541b3016a2bd294fc67e2e7258
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-02-18 10:49:02 -0600

    completed exercise

--- js/circle.js
+++ js/circle.js
@@ -9,15 +9,23 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
+             return Math.PI * (this.radius * 2);
 
-            return; // TODO: return the proper value
+             // TODO: return the proper value
+
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
+            if (doRounding) {
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea()))
+            } else {
 
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
+                // If doRounding is true, round the result to the nearest integer.
+                // Otherwise, output the complete value
+
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+            }
 
             console.log("Area of a circle with radius: " + this.radius + ", is: ");
         }
@@ -31,7 +39,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
